79cd3e25bc5a5e068c9e91b379d570b17647e8ba
Siddhartha45 <87509046+Siddhartha45@users.noreply.github.com>
2023-09-13 14:10:39 +0545

    excel file

--- report/views.py
+++ report/views.py
@@ -12,6 +12,7 @@ from .excel_data_mapping import *
 from fdip.decorators import superadmin_required, admin_required
 from industry.models import Industry
 from industry_without_gis.views import without_gis_data_import
+from industry_without_gis.excel_name import *
 
 
 @admin_required
@@ -88,74 +89,88 @@ def import_file(request):
 def gis_data_import(request, file):
     """function for exporting excel data of industries with gis info to Industry model"""
     df = pd.read_excel(file, dtype=dtype_mapping)
+    df.columns = df.columns.str.strip()     # Removes blank spaces from column names
+    
     for _, row in df.iterrows():
         industry_data = {}
         
-        if 'industry_name' in df.columns:
-            industry_name = row['industry_name']
-            if pd.notna(industry_name):
-                industry_data['industry_name'] = industry_name
+        if industry_name in df.columns:
+            industry_name_value = row[industry_name]
+            if pd.notna(industry_name_value):
+                industry_data['industry_name'] = industry_name_value
             else:                                                   # Excel row not having industry_name value are not entered
                 continue
 
-        if 'reg_date' in df.columns:
-            reg_date = row['reg_date']
-            if pd.notna(reg_date):
+        if reg_date in df.columns:
+            reg_date_value = row[reg_date]
+            if pd.notna(reg_date_value):
                 try:
-                    datetime_obj = pd.to_datetime(reg_date)
+                    datetime_obj = pd.to_datetime(reg_date_value)
                     formatted_reg_date = datetime_obj.strftime('%Y-%m-%d')  # Format as 'YYYY-MM-DD'
                     industry_data['reg_date'] = formatted_reg_date
                 except ValueError:
                     industry_data['reg_date'] = None
                     
-        column_names = ['industry_reg_no', 'owner_name', 'address', 'telephone_number', 'contact_person', 'mobile_number', 'ward_no', 
-                        'settlement', 'latitude', 'longitude', 'product_description', 'product_service_name', 'machinery_tool']
-        for column_name in column_names:
-            if column_name in df.columns:
-                value = row[column_name]
+        column_names = {
+            'industry_reg_no': industry_reg_no, 'owner_name': owner_name, 'address': address, 
+            'telephone_number': telephone_number, 'contact_person': contact_person, 
+            'mobile_number': mobile_number, 'ward_no': ward_no, 'settlement': settlement, 
+            'product_description': product_description, 'product_service_name': product_service_name, 
+            'machinery_tool': machinery_tool, 'latitude': latitude, 'longitude': longitude,
+        }
+        for k, v in column_names.items():
+            if v in df.columns:
+                value = row[v]
                 if pd.notna(value):
-                    industry_data[column_name] = value
+                    industry_data[k] = value
                     
         column_mappings = {
-            'sex': sex_mapping,
-            'caste': caste_mapping,
-            'investment': investment_mapping,
-            'industry_acc_product': industry_acc_product_mapping,
-            'current_status': current_status_mapping,
-            'ownership': ownership_mapping,
-            'raw_material_source': raw_materials_source_mapping,
-            'current_running_capacity': current_running_capacity_mapping,
-            'district': district_mapping,
-            'local_body': local_body_mapping,
+            'sex': (sex, sex_mapping),
+            'caste': (caste, caste_mapping),
+            'investment': (investment, investment_mapping),
+            'industry_acc_product': (industry_acc_product, industry_acc_product_mapping),
+            'current_status': (current_status, current_status_mapping),
+            'ownership': (ownership, ownership_mapping),
+            'raw_material_source': (raw_material_source, raw_materials_source_mapping),
+            'current_running_capacity': (current_running_capacity, current_running_capacity_mapping),
+            'district': (district, district_mapping),
+            'local_body': (local_body, local_body_mapping),
         }
         for column, mapping in column_mappings.items():
-            if column in df.columns:
-                choice = row[column]
+            if mapping[0] in df.columns:
+                choice = row[mapping[0]]
                 if pd.notna(choice):
-                    industry_data[column] = mapping.get(choice.strip(), None)
+                    industry_data[column] = mapping[1].get(choice.strip(), None)
 
-        manpower_columns = ['male', 'female', 'skillfull', 'unskilled', 'indigenous', 'foreign']
-        for manpower_column in manpower_columns:
-            if manpower_column in df.columns:
-                manpower_value = row[manpower_column] if pd.notna(row[manpower_column]) else 0
+        manpower_columns = {
+            'male': male, 'female': female, 'skillfull': skillfull, 
+            'unskilled': unskilled, 'indigenous': indigenous, 
+            'foreign': foreign
+        }
+        for k, v in manpower_columns.items():
+            if v in df.columns:
+                manpower_value = row[v] if pd.notna(row[v]) else 0
                 try:
                     int_manpower_value = int(manpower_value)
                 except ValueError:
                     int_manpower_value = 0
-                industry_data[manpower_column] = int_manpower_value
-        # Stores value of total manpower
-        total_manpower = industry_data['male'] + industry_data['female']
+                industry_data[k] = int_manpower_value
+        # Stores value of total manpower (if one of the column is missing returns 0)
+        total_manpower = industry_data.get('male', 0) + industry_data.get('female', 0)
         
-        capital_columns = ['yearly_capacity', 'fixed_capital', 'current_capital', 'total_capital']
-        for capital_column in capital_columns:
-            if capital_column in df.columns:
-                capital_value = row[capital_column] if pd.notna(row[capital_column]) else 0
+        capital_columns = {
+            'yearly_capacity': yearly_capacity, 'fixed_capital': fixed_capital, 
+            'current_capital': current_capital, 'total_capital': total_capital
+        }
+        for k, v in capital_columns.items():
+            if v in df.columns:
+                capital_value = row[v] if pd.notna(row[v]) else 0
                 clean_capital_value = str(capital_value).replace(',', '')  # Remove commas from the number
                 try:
                     float_capital_value = float(clean_capital_value)
                 except ValueError:
                     float_capital_value = 0
-                industry_data[capital_column] = float_capital_value
+                industry_data[k] = float_capital_value
         
         if industry_data:
             industry = Industry(**industry_data)
